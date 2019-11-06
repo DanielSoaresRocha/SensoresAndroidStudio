9553ac9524163679b8206e956137670590c4c171
Daniel Soares <legoliga123@gmail.com>
2019-11-06 10:02:19 -0300

    Sensor de proximidade adicionado

---
+++ Sensores/app/src/main/java/com/example/sensores/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.sensores
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var mSensorManager : SensorManager? = null
+    private var mProx : Sensor? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+        mProx = mSensorManager?.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        mSensorManager?.registerListener(ProxSensor(),mProx,SensorManager.SENSOR_DELAY_FASTEST)
+
+
+    }
+
+    internal inner class ProxSensor : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent) {
+            val vl = event.values[0]
+            Log.i("SENSORPROXIMIDADE", "VALOR DO SENSOR = "+ vl)
+
+            if (vl >= 10) {
+                imgDrake.setImageResource(R.drawable.drake2)
+            } else {
+                imgDrake.setImageResource(R.drawable.drake)
+            }
+
+        }
+    }
+
+
+}
